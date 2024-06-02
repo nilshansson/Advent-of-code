7efd6f7d10711a5dc343fb7f4ddd9e74f3f6ade3
Nils Hansson <nilsmenghansson@gmail.com>
2024-06-02 15:29:27 +0200

    created array for part 2

---
+++ day1/part2.ts
@@ -0,0 +1,40 @@
+import { readFile } from "node:fs";
+
+const getData = () => {
+  return readFile("day1/data.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const stringData = data.split("\n");
+
+    const regex = /\d/g;
+    const lineNumbers: string[] = [];
+    for (let line of stringData) {
+      lineNumbers.push(line.match(regex)!.join(""));
+    }
+
+    let twoNumbersTogether = "";
+    let pairStringNumbersArr: string[] = [];
+
+    for (let numberLine of lineNumbers) {
+      if (numberLine.length === 1) {
+        twoNumbersTogether = numberLine + numberLine;
+      } else {
+        twoNumbersTogether = numberLine[0] + numberLine[numberLine.length - 1];
+      }
+      pairStringNumbersArr.push(twoNumbersTogether);
+    }
+    let pairNumbersArr: number[] = [];
+    for (const pair of pairStringNumbersArr) {
+      pairNumbersArr.push(parseFloat(pair));
+    }
+
+    console.log(
+      pairNumbersArr.reduce((sum, currentValue) => sum + currentValue)
+    );
+  });
+};
+
+getData();
